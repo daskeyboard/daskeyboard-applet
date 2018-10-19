43be52e6df6dddb87472584a514c9a93cc991f2f
corykim <cory.kim@gmail.com>
2018-10-19 10:29:46 -0500

    chore(eslint): Add eslint

--- index.js
+++ index.js
@@ -1,54 +1,150 @@
-const request = require('request-promise');
-const utility = require('./lib/utility');
-const Storage = require('node-storage');
+import { post } from 'request-promise';
+import Storage from 'node-storage';
+import { mergeDeep } from './lib/utility';
 
 const signalHeaders = {
-  "Content-Type": "application/json"
-}
+  'Content-Type': 'application/json',
+};
 
 const defaultPortInterval = 2000;
+
+const backendUrl = 'http://localhost:27301';
+const signalEndpoint = `${backendUrl}/api/2.0/signals`;
 
 /**
  * The root of all configuration for this extension
  */
-var rootConfig;
+let rootConfig;
 
 /**
  * The instance ID of the extension
  */
-var extensionId;
+let extensionId;
 
 /**
  * The configuration of the applet itself
  */
-var appletConfig;
+let appletConfig;
 
 /**
  * The extension's authorization info
  */
-var authorization;
+let authorization;
 
 /**
  * The extension's geometry
  */
-var geometry;
+let geometry;
 
 /**
  * The location of the extension's local storage
  */
-var storageLocation;
+let storageLocation;
+
+
+/**
+ * Read the configuration from command line arguments. The first command line
+ * argument should be a JSON string.
+ */
+function readConfig() {
+  let result = null;
+  if (process.argv.length > 2) {
+    try {
+      const config = JSON.parse(process.argv[2]);
+      result = Object.freeze(config);
+    } catch (error) {
+      console.error(`Could not parse config as JSON: ${process.argv[2]}`);
+      process.exit(1);
+    }
+  } else {
+    result = Object.freeze({});
+  }
+
+  return result;
+}
+
+function applyConfig(config) {
+  rootConfig = Object.freeze(config || readConfig());
+  ({ extensionId, geometry, storageLocation } = rootConfig);
+  appletConfig = Object.freeze(mergeDeep({},
+    rootConfig.applet.defaults || {}, rootConfig.applet.user || {}));
+  authorization = rootConfig.authorization || {};
+}
 
 applyConfig();
 
 
-function applyConfig(rootConfig) {
-  rootConfig = Object.freeze(rootConfig ? rootConfig : readConfig());
-  extensionId = rootConfig.extensionId;
-  appletConfig = Object.freeze(utility.mergeDeep({}, rootConfig.applet.defaults || {}, rootConfig.applet.user || {}));
-  authorization = rootConfig.authorization || {};
-  geometry = rootConfig.geometry;
-  storageLocation = rootConfig.storageLocation;
+/**
+ * Send a signal to the local Das Keyboard Q Service.
+ * @param {Signal} signal
+ */
+async function sendLocal(signal) {
+  if (!geometry || !geometry.origin) {
+    throw new Error('Geometry is not properly defined:', geometry);
+  } else {
+    const originX = geometry.origin.x || 0;
+    const originY = geometry.origin.y || 0;
+
+    const actionValue = [];
+
+    const rows = signal.points;
+    for (let y = 0; y < rows.length; y += 1) {
+      const columns = rows[y];
+      for (let x = 0; x < columns.length; x += 1) {
+        const point = columns[x];
+        actionValue.push({
+          zoneId: `${originX + x},${originY + y}`,
+          effect: point.effect,
+          color: point.color,
+        });
+      }
+    }
+
+    const body = {
+      action: 'DRAW',
+      actionValue: JSON.stringify(actionValue),
+      pid: 'Q_MATRIX',
+      message: signal.options.message || '',
+      name: signal.options.name || 'Q Desktop Signal',
+      isMuted: !signal.options.message,
+      clientName: extensionId,
+    };
+
+    // console.log('Posting to local service:', JSON.stringify(body));
+
+    return post({
+      uri: signalEndpoint,
+      headers: signalHeaders,
+      body,
+      json: true,
+    }).then((json) => {
+      // no-op on successful completion
+    }).catch((err) => {
+      const error = err.error;
+      if (error.code === 'ECONNREFUSED') {
+        console.error(`Error: failed to connect to ${signalEndpoint}, make sure` +
+          ` the Das Keyboard Q software  is running`);
+      } else {
+        console.error('Error sending signal ', error);
+      }
+    });
+  }
 }
+
+/**
+ * An enumeration of effects
+ */
+export const Effects = Object.freeze({
+  SET_COLOR: 'SET_COLOR',
+  BLINK: 'BLINK',
+  BREATHE: 'BREATHE',
+  COLOR_CYCLE: 'COLOR_CYCLE',
+  RIPPLE: 'RIPPLE',
+  INWARD_RIPPLE: 'INWARD_RIPPLE',
+  BOUNCING_LIGHT: 'BOUNCING_LIGHT',
+  LASER: 'LASER',
+  WAVE: 'WAVE',
+});
 
 
 /**
@@ -61,70 +157,67 @@ class QDesktopApp {
     this.extensionId = extensionId;
     this.store = new Storage(storageLocation);
 
-    console.log("Constructing app with config: ", this.config);
-    console.log("Constructing app with geometry: ", geometry);
+    console.log('Constructing app with config: ', this.config);
+    console.log('Constructing app with geometry: ', geometry);
 
-    process.on('SIGINT', (message) => {
+    process.on('SIGINT', () => {
       this.shutdown();
       process.exit();
-    })
+    });
 
     this.pollingInterval = defaultPortInterval;
     this.pollingBusy = false;
     this.errorState = null;
 
-    process.on('message', (m) => this.handleMessage(m));
-    console.log("Constructor finished.");
+    process.on('message', m => this.handleMessage(m));
+    console.log('Constructor finished.');
   }
 
   async handleMessage(m) {
     if (m.startsWith('{')) {
       const message = JSON.parse(m);
-      console.log("CHILD Received JSON message: ", message);
+      console.log('CHILD Received JSON message: ', message);
 
-      const type = message.type;
-      const data = message.data;
+      const { type, data } = message;
       switch (type) {
         case 'CONFIGURE': {
-          console.log("Reconfiguring: " +  JSON.stringify(data));
+          console.log(`Reconfiguring: ${JSON.stringify(data)}`);
           applyConfig(Object.freeze(data));
           break;
         }
         case 'SELECTIONS':
-          {
-            console.log("CHILD Handling " + type);
-            this.selections(data.fieldName).then(selections => {
-              console.log("CHILD returned selections.");
-              const response = {
-                type: 'SELECTIONS',
-                data: selections
-              }
-              process.send(JSON.stringify(response));
-            });
-            break;
-          }
+        {
+          console.log(`CHILD Handling ${type}`);
+          this.selections(data.fieldName).then((selections) => {
+            console.log('CHILD returned selections.');
+            const response = {
+              type: 'SELECTIONS',
+              data: selections,
+            };
+            process.send(JSON.stringify(response));
+          });
+          break;
+        }
         default:
-          {
-            console.error("Don't know how to handle JSON message of type: '" + type + "'");
-          }
+        {
+          console.error(`Don't know how to handle JSON message of type: '${type}'`);
+        }
       }
     } else {
       switch (m) {
         case 'START':
-          {
-            console.log("Got START");
-            this.start();
-            break;
-          }
+        {
+          console.log('Got START');
+          this.start();
+          break;
+        }
         default:
-          {
-            console.error("Don't know what to do with message: '" + m + "'");
-          }
+        {
+          console.error(`Don't know what to do with message: '${m}'`);
+        }
       }
     }
   }
-
-
 
 
   /**
@@ -136,18 +229,17 @@ class QDesktopApp {
 
     setInterval(() => {
       this.poll();
-
     }, this.pollingInterval);
   }
 
 
   /**
-   * Schedules the run() function at regular intervals. Currently set to a 
+   * Schedules the run() function at regular intervals. Currently set to a
    * constant value, but may become dynamic in the future.
    */
   poll() {
     if (this.pollingBusy) {
-      console.log("Skipping run because we are still busy.");
+      console.log('Skipping run because we are still busy.');
     } else {
       this.pollingBusy = true;
       try {
@@ -162,7 +254,8 @@ class QDesktopApp {
       } catch (error) {
         this.errorState = error;
         console.error(
-          "Applet encountered an uncaught error in its main loop", error);
+          'Applet encountered an uncaught error in its main loop', error
+);
         this.pollingBusy = false;
       }
     }
@@ -187,19 +280,17 @@ class QDesktopApp {
    * Given an (optional) fieldName, return the valid selections for that field
    * name. This is used to generate a UI to allow the user to configure the
    * applet.
-   * @param {string} fieldName 
+   * @param {string} fieldName
    * @returns {Object} an array of [key, value] pairs
    */
-  async selections(fieldName) {
-
-  }
+  async selections(fieldName) {}
 }
 
 
 /**
  * Class representing a single point to be sent to the device
  * @param {string} color - The hexadecimal RGB color to activate, e.g. '#FFCCDD'
- * @param {string} effect - The effect to activate. Enumerated in Effects. 
+ * @param {string} effect - The effect to activate. Enumerated in Effects.
  *   Default is empty.
  */
 class QPoint {
@@ -211,120 +302,19 @@ class QPoint {
 
 class QDesktopSignal {
   /**
-   * 
+   *
    * @param {QPoint[][]} points A 2D array of QPoints expressing the signal
    * @param {*} options A JSON list of options
    */
   constructor(points, options) {
     this.points = points;
-    this.options = options || {}
+    this.options = options || {};
     this.extensionId = extensionId;
   }
 }
 
 
-/**
- * An enumeration of effects
- */
-const Effects = Object.freeze({
-  'SET_COLOR': 'SET_COLOR',
-  'BLINK': 'BLINK',
-  'BREATHE': 'BREATHE',
-  'COLOR_CYCLE': 'COLOR_CYCLE',
-  'RIPPLE': 'RIPPLE',
-  'INWARD_RIPPLE': 'INWARD_RIPPLE',
-  'BOUNCING_LIGHT': 'BOUNCING_LIGHT',
-  'LASER': 'LASER',
-  'WAVE': 'WAVE'
-});
-
-const backendUrl = 'http://localhost:27301';
-const signalEndpoint = backendUrl + '/api/2.0/signals';
-
-/**
- * Send a signal to the local Das Keyboard Q Service.
- * @param {Signal} signal 
- */
-async function sendLocal(signal) {
-  if (!geometry || !geometry.origin) {
-    console.error("Geometry is not properly defined:", geometry);
-  } else {
-    const originX = geometry.origin.x || 0;
-    const originY = geometry.origin.y || 0;
-
-    const actionValue = [];
-
-    const rows = signal.points;
-    for (let y = 0; y < rows.length; y++) {
-      const columns = rows[y];
-      for (let x = 0; x < columns.length; x++) {
-        const point = columns[x];
-        actionValue.push({
-          zoneId: (originX + x) + ',' + (originY + y),
-          effect: point.effect,
-          color: point.color
-        });
-      }
-    }
-
-    const body = {
-      action: "DRAW",
-      actionValue: JSON.stringify(actionValue),
-      pid: "Q_MATRIX",
-      message: signal.options.message || "",
-      name: signal.options.name || "Q Desktop Signal",
-      isMuted: !signal.options.message,
-      clientName: extensionId
-    }
-
-    //console.log("Posting to local service:", JSON.stringify(body));
-
-    return request.post({
-      uri: signalEndpoint,
-      headers: signalHeaders,
-      body: body,
-      json: true
-    }).then(function (json) {
-      // no-op on successful completion
-    }).catch(function (err) {
-      const error = err.error;
-      if (error.code === 'ECONNREFUSED') {
-        console.error(`Error: failed to connect to ${signalEndpoint}, make sure` +
-          ` the Das Keyboard Q software  is running`);
-      } else {
-        console.error('Error sending signal ', error);
-      }
-    });
-
-  }
-}
-
-
-/**
- * Read the configuration from command line arguments. The first command line 
- * argument should be a JSON string.
- */
-function readConfig() {
-  if (process.argv.length > 2) {
-    try {
-      let config = JSON.parse(process.argv[2]);
-      Object.freeze(config);
-      return config;
-    } catch (error) {
-      console.error("Could not parse config as JSON: " + process.argv[2]);
-      process.exit(1);
-    }
-  } else {
-    return Object.freeze({});
-  }
-}
-
-
-
-module.exports = {
-  DesktopApp: QDesktopApp,
-  Point: QPoint,
-  Send: sendLocal,
-  Signal: QDesktopSignal,
-  Effects: Effects
-}
+export const DesktopApp = QDesktopApp;
+export const Point = QPoint;
+export const Send = sendLocal;
+export const Signal = QDesktopSignal;
